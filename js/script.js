ff0bbb21eb1651b1abc79a6473e3db289a79ff12
VoronWorks <anastasiya.voronova3@gmail.com>
2023-01-21 19:28:41 +0300

    Added form submission

--- js/script.js
+++ js/script.js
@@ -134,3 +134,23 @@ function faq12(){
         q.classList.add("question-selected");
     }
 }
+
+$(function(){
+    $(".ajaxForm").submit(function(e){
+        e.preventDefault();
+        var href = $(this).attr("action");
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: href,
+            data: $(this).serialize(),
+            success: function(response){
+                if(response.status == "success"){
+                    alert("Заявка была успешно отправлена!");
+                }else{
+                    alert("Произошла ошибка... " + response.message);
+                }
+            }
+        });
+    });
+});
